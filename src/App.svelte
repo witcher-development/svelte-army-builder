459c830d8d574a7e72037ba88a99c285867930eb
Witcher <roman.web.kuznietsov@gmail.com>
2019-12-05 18:49:39 +0200

    enable routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
+	import { Router, Link, Route, navigate } from 'svelte-routing';
 	import bgi from 'assets/bgi.jpg';
+
+	import Login from './components/Login.svelte';
+	import Desk from './components/Desk.svelte';
+
 	import myStore from './store';
 
 	const name: string = 'test';
@@ -8,11 +13,18 @@
 	onMount(() => {
 		console.log('App mounted');
 	});
+
+	const login = () => {
+		navigate('/');
+	};
 </script>
 
 <style type="text/scss">
 	.app {
-		/*background-image: url(assets/bgi.jpg);*/
+		height: 100%;
+
+		background-image: url(assets/bgi.jpg);
+		background-size: cover;
 		h1 {
 			color: #fff;
 		}
@@ -20,6 +32,18 @@
 </style>
 
 <div class="app">
-		<img src={bgi} alt="">
-	<h1>Hello {name}!</h1>
+	<div>
+		<h1>Hello {name}!</h1>
+		<button on:click={login}>Log in</button>
+	</div>
+	<Router>
+		<nav>
+			<Link to="/">Home</Link>
+			<Link to="login">login</Link>
+		</nav>
+		<div>
+			<Route path="login" component={Login} />
+			<Route path="/" component={Desk} />
+		</div>
+	</Router>
 </div>
